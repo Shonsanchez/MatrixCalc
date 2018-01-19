f7d3c72a59190b7cc3ef47b88a15aa57fa4414c1
Shon Sanchez <shon.d.sanchez@gmail.com>
2018-01-19 16:40:39 -0500

    Files updated.

--- MatrixOp.c
+++ MatrixOp.c
@@ -4,8 +4,16 @@
 #include <stdlib.h>
 #include "MatrixOp.h"
 
+Matrix* createMatrix(int row, int col){
+    Matrix* m = malloc(sizeof(Matrix*));
+    m->matrix = malloc(sizeof(int**)*col);
+    for (int i = 0; i < col; ++i) {
+        m->matrix[i] = malloc(sizeof(int*) *row);
+    }
+    return m;
+}
 
-int** addMatrix(int** m1, int** m2, int row, int col){
-    int** m3 = malloc(sizeof(int*) * row);
+Matrix* addMatrix(Matrix* m1, Matrix* m2){
+    Matrix* m3 = createMatrix(m1->rowC,m2->colC);
     return m3;
 }
--- MatrixOp.h
+++ MatrixOp.h
@@ -11,7 +11,7 @@ typedef struct{
     int curRow; // Current row
     int colC; //Column count
     int curCol; // Current column
-    int** grid; //the matrix
+    int** matrix; //the matrix
 }Matrix;
 
 Matrix* createMatrix(int row, int col);
@@ -21,30 +21,30 @@ Matrix* createMatrix(int row, int col);
  * @param m2  - the second matrix
  * @return the summation of the matrices
  */
-int** addMatrix(Matrix* m1, Matrix* m2);
+Matrix* addMatrix(Matrix* m1, Matrix* m2);
 
 /**Subtracts the entries in row,col from m1 by m2
  * @param m1 - the first matrix
  * @param m2 - the second matrix
  * @return - the subtraction of the matrix
  */
-int** subMatrix(Matrix* m1, Matrix* m2);
+Matrix* subMatrix(Matrix* m1, Matrix* m2);
 
 /**Multiplies two matrices
  * @param m1 - the first matrix
  * @param m2 - the second matrix
  * @return - the multiplication of the matrices
  */
-int** multMatrix(Matrix* m1, Matrix* m2);
+Matrix* multMatrix(Matrix* m1, Matrix* m2);
 
 
-int** inverseM(Matrix* m);
+Matrix* inverseM(Matrix* m);
 
 /**
  * Takes the rows of the matrix and convert them into columns.
  * @param m- the matrix to transpose
  * @return - the transpose of the matrix
  */
-int** transposeM(Matrix* m);
+Matrix* transposeM(Matrix* m);
 
 #endif; //MATRIXCALC_MATRIXOP_H
