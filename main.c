27b65efa164b195d6770c1a9ce4fd3c5da422332
Shon Sanchez <shon.d.sanchez@gmail.com>
2018-01-19 12:19:13 -0500

    Takes user input. Asks for the operation to be taken and the row and column count.

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int choice = 0;
+    int row;
+    int col;
+    int** matrix;
+    int*** matrices;
+    printf("Welcome to MatrixCalc.\n");
+    while(choice > 5 || choice < 1){
+        printf("Please select an option\n");
+        printf("1)Addition\n"
+                       "2)Subtraction\n"
+                       "3)Transpose\n"
+                       "4)inverse\n"
+                       "5)Muliplication\n");
+        scanf("%i",&choice);
+    }
+    printf("Enter the number of rows and column in your matrix (ex:3 2)\n");
+    scanf("%i %i", &row, &col);
+    printf("You have selected a %i by %i matrix\n", row,col);
     return 0;
 }
